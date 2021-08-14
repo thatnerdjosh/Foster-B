c864f4a3ca5fd6bd20bd4b01c18259e14b37e891
Josh Santos <josh@omnidapps.com>
2021-08-14 04:09:54 +0000

    Extract out docker clean logic to go along with clean action

--- workspace/makefile.controls/build.sh
+++ workspace/makefile.controls/build.sh
@@ -15,37 +15,42 @@ ws_mount="/opt/foster"
 # The name of the base image we will create
 IMAGE_NAME="foster-standalone-prealpha"
 
+# Determines if we are working from a clean env
+function is_clean_env
+{
+  [ !$(docker ps -a | grep "$IMAGE_NAME") ]
+}
+
 # We want to orchestrate the docker activity as this is not a web application
 # hosting model, we're just using docker for environment boundary
 function orchestrate_docker 
 {
-	# Remove old container
-	docker image rm "${IMAGE_NAME}" 2>/dev/null
-	
 	# Rebuild the base Docker image
-	docker build --no-cache -t "${IMAGE_NAME}" $workspace/
+	docker build -t "${IMAGE_NAME}" $workspace/
 
-	# Run the build container and map /foster_workspace as a volume
-	docker run \
-		--cpus $(grep -c ^processor /proc/cpuinfo) \
-		--rm \
-		-h "${IMAGE_NAME}" \
-		--memory-swappiness=0 \
-		-m $(free -m | grep Mem | awk '{ print $4; }')m \
-		--privileged \
-		-ti \
-		-v $(realpath $workspace):${ws_mount}:Z \
-		--name "${IMAGE_NAME}" \
-		-d ${IMAGE_NAME}:latest
+  # If clean, start new container
+  if is_clean_env; then
+    # Run the build container and map /foster_workspace as a volume
+    docker run \
+      --cpus $(grep -c ^processor /proc/cpuinfo) \
+      -h "${IMAGE_NAME}" \
+      --memory-swappiness=0 \
+      -m $(free -m | grep Mem | awk '{ print $4; }')m \
+      --privileged \
+      -ti \
+      -v $(realpath $workspace):${ws_mount}:Z \
+      --name "${IMAGE_NAME}" \
+      -d ${IMAGE_NAME}:latest
+  # otherwise, start existing one
+  else
+    docker start ${IMAGE_NAME}
+  fi
 
 	# Execute the init script
 	docker exec -it "${IMAGE_NAME}" /opt/foster/init.sh
 
 	# Stop the container
 	docker stop ${IMAGE_NAME}
-
-	# delete the container
-	docker rm ${IMAGE_NAME}	
 }
 
 # We'll want to share a host directory with the container as the work inside our
--- workspace/makefile.controls/clean.sh
+++ workspace/makefile.controls/clean.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# The name of the base image we created
+IMAGE_NAME="foster-standalone-prealpha"
+
 rm -Rfv $workspace/output
 rm -Rfv $workspace/sources/cache
 sudo rm -Rfv $workspace/logs
+
+# delete the container
+docker rm ${IMAGE_NAME}
+
+# Remove old container image
+docker image rm "${IMAGE_NAME}" 2>/dev/null
